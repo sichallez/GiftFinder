dcd237e25a38a7e86cb87fe0bf71067958bbf7d4
yahsuanlinus <savannahwang75@gmail.com>
2022-05-31 15:31:51 +0800

    tweak api users format

--- server/api/users.js
+++ server/api/users.js
@@ -1,19 +1,21 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
 // Route "/api/users"
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
+      attributes: ["id", "username"],
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
