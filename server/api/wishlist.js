559f7406040c84ff4beaa63fd67eb8c941562a03
yahsuanlinus <savannahwang75@gmail.com>
2022-05-31 15:32:09 +0800

    tweak api wishlist format

--- server/api/wishlist.js
+++ server/api/wishlist.js
@@ -1,33 +1,28 @@
 const router = require("express").Router();
 const {
-  models: { Wishlist, User},
+  models: { Wishlist, User },
 } = require("../db");
 
-
 router.get("/", async (req, res, next) => {
-    try{
-        const user = await User.findByToken(req.headers.authorization);
+  try {
+    const user = await User.findByToken(req.headers.authorization);
 
-        console.log('USER');
+    console.log("USER");
 
-        console.log(req.headers);
+    console.log(req.headers);
 
-        const wishlist = await Wishlist.findOne({
-          where: {
-            userId: user.id
-          }
-        })
+    const wishlist = await Wishlist.findOne({
+      where: {
+        userId: user.id,
+      },
+    });
 
-
-        res.send(wishlist);
-      }
-      catch(err){
-        if (err.status === 401){
-          res.sendStatus(401);
-        }
-        else
-          next(err);
-      }
+    res.send(wishlist);
+  } catch (err) {
+    if (err.status === 401) {
+      res.sendStatus(401);
+    } else next(err);
+  }
 });
 
 module.exports = router;
