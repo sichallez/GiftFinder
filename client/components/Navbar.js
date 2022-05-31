70b1ccf2b7f5b67945b32b7b2ad75edd74681cad
Ying Ying <yingyingfengg@gmail.com>
2022-05-30 22:31:02 -0400

    css styling

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,25 +3,51 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = ({handleClick, isLoggedIn, username}) => (
   <div>
-    <h1>FS-App-Template</h1>
     <nav>
       {isLoggedIn ? (
-        <div>
+        <div className='nav-flex'>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <Link to='/wishlist'>Wishlist</Link>
+          <div className='nav-flex-item'>
+            <ul className='nav-top-level'>
+              <li className='nav-title'><Link to="/home">GiftFinder</Link></li>
+            </ul>
+          </div>
+          <div className='nav-flex-item'>
+            <ul className='nav-top-level'>
+              <li>
+                <ul className='nav-top-level'>
+                  <li>{username}</li>
+                  <li><Link to='/wishlist'>Wishlist</Link></li>
+                  <li>
+                    <a href='#' onClick={handleClick}>
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       ) : (
-        <div>
+        <div className='nav-flex'>
           {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <div className='nav-flex-item'>
+            <ul className='nav-top-level'>
+              <li className='nav-title'><Link to="/home">GiftFinder</Link></li>
+            </ul>
+          </div>
+          <div className='nav-flex-item'>
+            <ul className='nav-top-level'>
+              <li>
+                <ul className='nav-top-level'>
+                  <li><Link to='login'>Login</Link></li>
+                  <li><Link to='signup'>Sign Up</Link></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </nav>
@@ -34,7 +60,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.auth.id
+    isLoggedIn: !!state.auth.id,
+    username: state.auth.username,
   }
 }
 
