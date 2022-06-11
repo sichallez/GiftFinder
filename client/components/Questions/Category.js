4b5a6d99634b2d0a9371a3f192110c7ca66e96ff
Ying Ying <yingyingfengg@gmail.com>
2022-06-11 12:12:00 -0400

    some css

--- client/components/Questions/Category.js
+++ client/components/Questions/Category.js
@@ -1,34 +1,70 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { setCategory } from '/client/store';
 import ReturnAndContinue from './QuestionsComponents/ReturnAndContinue'
 
 const Category = ({ category }) => {
+    const categories = [
+    //     'Clothes',
+    //     'Electronics',
+    //     'Home & Living',
+    //     'Sports & Outdoors',
+    //     'Games',
+    //     'Craft Supplies',
+    //     'Books',
+    //     'Kitchen & Bar',
+    //     'Jewelry & Accessories',
+    //     'Anniversary',
+    //     'All'
+    // ]
+      {id: 1, name: 'Clothes'},
+      {id: 2, name: 'Electronics'},
+      {id: 3, name: 'Home & Living'},
+      {id: 4, name: 'Sports & Outdoors'},
+      {id: 5, name: 'Games'},
+      {id: 6, name: 'Craft Supplies'},
+      {id: 7, name: 'Books'},
+      {id: 8, name: 'Kitchen & Bar'},
+      {id: 9, name: 'Jewelry & Accessories'},
+      {id: 10, name: 'Anniversary'},
+      {id: 11, name: 'All'}
+    ]
+
     const initialCategory = category
     const [selectedCategory, setSelectedCategory] = useState(initialCategory)
+
     function toggleSelected(key) {
-        console.log(key)
+        if (selectedCategory.includes(key)) {
+            const updatedSelection = [...selectedCategory]
+            updatedSelection.splice(updatedSelection.indexOf(key), 1)
+            setSelectedCategory(updatedSelection)
+        } else {
+            setSelectedCategory([...selectedCategory, key])
+        }
     }
+    console.log(selectedCategory, 'selected', categories, 'cat')
     return (
         <div>
             <h1 className='questionsH1'> Choose a category</h1>
-            {category.map((cat) => {
-                return (
-                    <button
-                        className='category-btn'
-                        key={cat.id}
-                        value={cat.name}
-                        onClick={() => {
-                            toggleSelected(cat.name)
-                        }}
-                    >{cat.name}</button>
-                )
-            })}
+            <div className='category-div'>
+                {categories.map((cat) => {
+                    return (
+                        <button
+                            className={cat.id === cat.id ? 'category-btn' : 'category-btnActive'}
+                            key={cat.id}
+                            value={cat.name}
+                            onClick={() => {
+                                toggleSelected(cat.name)
+                            }}
+                        >{cat.name}</button>
+                    )
+                })}
+            </div>
             <ReturnAndContinue
             returnPath={'/questions/character'}
             continuePath={'/questions/result'}
             onContinueClick = {() => setCategory(selectedCategory)}
-        />
+            />
         </div>
     )
 }
